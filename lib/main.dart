3b8209a5a2ff1f7d388945a68364ef1b8edaca13
carmelbar24 <95103@ganyavne.ort.org.il>
2023-03-02 09:32:53 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+import 'mainPage.dart';
+import 'mainPage2.dart';
+import 'main_page_interactive.dart';
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home:mainPage2()
+    );
+  }
+}
+
